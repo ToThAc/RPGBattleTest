718ed73213fca501488fc72ccb2db7b7044c825a
ToThAc <39631109+ToThAc@users.noreply.github.com>
2020-06-16 16:36:28 -0500

    Created working multiplayer. Fixes #8 Fixes #9

--- battle.py
+++ battle.py
@@ -3,12 +3,13 @@ import random
 import time
 
 class Character:
-	def __init__(self):
+	def __init__(self,identifier):
 		self.hitpoints = 1000
 		self.attack = random.randint(20,30)
 		self.defense = random.randint(10,20)
 		self.attacks = set()
 		self.items = set()
+		self.identifier = identifier
 	def damage(self,hp):
 		self.hitpoints -= hp
 	def heal(self,h):
@@ -33,10 +34,10 @@ def validinput(commandstring,validlist):
 itemmasterlist = {"HEART":Heart(),"SUPER HEART":Heart(60),"ULTRA HEART":Heart(90),"MAX HEART":Heart(120)}
 command = ''
 
-def playerattackprompt(promptstring,damagedealt):
+def playerattackprompt(promptstring,damagedealt,enemy):
     print(promptstring)
     time.sleep(2)
-    print("...and inflicted", damagedealt, "damage to ENEMY!")
+    print("...and inflicted", damagedealt, f"damage to ENEMY {enemy.identifier}!")
     enemy.damage(damagedealt)
     time.sleep(1)
 
@@ -45,10 +46,10 @@ def playerhealprompt(item):
     print(f"You restored {item.hp} HP!")
     time.sleep(1)
 
-def enemyattackprompt(promptstring,damagedealt):
+def enemyattackprompt(promptstring,damagedealt,player):
     print(promptstring)
     time.sleep(2)
-    print("...and thus you received", damagedealt, "damage!")
+    print(f"...and thus PLAYER {player.identifier} received", damagedealt, "damage!")
     player.damage(damagedealt)
     time.sleep(1)
 
@@ -71,22 +72,29 @@ class EnemyAttack(Attack):
         self.constant = random.randint(30, 70)
 
 class PlayerCharacter(Character):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,identifier):
+        super().__init__(identifier)
         self.attacks = {x:PlayerAttack(x,y) for (x,y) in [["SLASH","You swung your blade..."], ["FIREBALL","You hurled a fireball..."], ["ICE CRYSTAL","You chucked an icicle..."]]}
         self.items = [i for i in itemmasterlist] * 5
     def taketurn(self,defense):
         while True:
+            string = f"Will PLAYER {self.identifier} ATTACK, use ITEMS, or FLEE? "
             command = validinput(string,("ATTACK", "ITEMS", "FLEE"))
             if command == "ATTACK":
                 attack = validinput(attackstring,list(self.attacks.keys()) + ["BACK"])
                 if attack in self.attacks:
-                    playerattackprompt(self.attacks[attack].quote,self.attacks[attack].calcdamage(self.attack,defense))
+                    choice = validinput(whichenemy,[str(i.identifier) for i in enemies] + ["BACK"])
+                    if choice == "BACK":
+                        continue
+                    for enemy in enemies:
+                        if int(choice) == enemy.identifier:
+                            break
+                    playerattackprompt(self.attacks[attack].quote,self.attacks[attack].calcdamage(self.attack,enemy.defense),enemy)
                     return True
                 if attack == "BACK":
                     continue
             elif command == "ITEMS":
-                itemstring = f"Which item will you choose: HEART (×{self.items.count('HEART')}), SUPER HEART (×{self.items.count('SUPER HEART')}), ULTRA HEART (×{self.items.count('ULTRA HEART')}), or MAX HEART (×{self.items.count('MAX HEART')})? (You can also type \"BACK\" to go back.) "
+                itemstring = f"Which item will PLAYER {self.identifier} choose: HEART (×{self.items.count('HEART')}), SUPER HEART (×{self.items.count('SUPER HEART')}), ULTRA HEART (×{self.items.count('ULTRA HEART')}), or MAX HEART (×{self.items.count('MAX HEART')})? (You can also type \"BACK\" to go back.) "
                 items = validinput(itemstring,list(itemmasterlist.keys()) + ["BACK"])
                 while items not in self.items:
                     if items in itemmasterlist:
@@ -115,38 +123,49 @@ class PlayerCharacter(Character):
                 return False
 
 class EnemyCharacter(Character):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,identifier):
+        super().__init__(identifier)
         self.attacks = {x:EnemyAttack(x,y) for (x,y) in [["BITE","ENEMY latches its jaws onto you..."], ["STOMP","ENEMY raises its foot onto you..."], ["SMASH","ENEMY charges up to ram into you..."]]}
     def taketurn(self,defense):
-        print("ENEMY readies an attack!")
+        enemystring = f"ENEMY {self.identifier} readies an attack!"
+        print(enemystring)
         time.sleep(1)
         enemychoice = random.choice(list(self.attacks.keys()))
         if enemychoice in self.attacks:
-            enemyattackprompt(self.attacks[enemychoice].quote,self.attacks[enemychoice].calcdamage(self.attack,defense))
+            player = random.choice(players)
+            enemyattackprompt(self.attacks[enemychoice].quote,self.attacks[enemychoice].calcdamage(self.attack,player.defense),player)
 
-player = PlayerCharacter()
-enemy = EnemyCharacter()
-
-def hpstatistics():
-    print(f"ENEMY has {enemy.hitpoints}/1000 HP.")
-    time.sleep(1)
-    print(f"You currently have {player.hitpoints}/1000 HP.")
-    time.sleep(1)
+def hpstatistics(enemies,players):
+    for enemy in enemies:
+        print(f"ENEMY {enemy.identifier} has {enemy.hitpoints}/1000 HP.")
+        time.sleep(1)
+    for player in players:
+        print(f"PLAYER {player.identifier} currently has {player.hitpoints}/1000 HP.")
+        time.sleep(1)
 
 def defeatstate():
-    if enemy.hitpoints <= 0:
-        print("ENEMY is defeated!")
+    for enemy in enemies:
+        if enemy.hitpoints <= 0:
+            enemies.remove(enemy)
+            print(f"ENEMY {enemy.identifier} is defeated!")
+            time.sleep(1)
+    for player in players:
+        if player.hitpoints <= 0:
+            players.remove(player)
+            print(f"PLAYER {player.identifier} is defeated!")
+            time.sleep(1)
+    if len(enemies) == 0:
+        print("All ENEMIES defeated!")
         time.sleep(1)
         print("YOU WIN!")
         time.sleep(2)
-        print("You obtained 43 EXP.")
+        print("Your team obtained 43 EXP.")
         time.sleep(1)
         return True
-    elif player.hitpoints <= 0:
-        print("You were defeated!")
+    elif len(players) == 0:
+        print("Your team was defeated!")
         time.sleep(1)
-        print("You lost the battle...")
+        print("Your team lost the battle...")
         time.sleep(2)
         print("GAME OVER")
         time.sleep(1)
@@ -155,19 +174,24 @@ def defeatstate():
         return False
 
 # Start battle
-print("ENEMY attacks!")
+quantity = int(input("How many players? "))
+print("ENEMY team attacks!")
+players = [PlayerCharacter(i+1) for i in range(quantity)]
+enemies = [EnemyCharacter(i+1) for i in range(quantity)]
 time.sleep(1)
-hpstatistics()
-string = "Will you ATTACK, use ITEMS, or FLEE? "
+hpstatistics(enemies,players)
 errormessage = "Command not recognized. Try again."
 itemrelinquish = "You're out of that particular item..."
 attackstring = "Which attack will it be: SLASH, FIREBALL, or ICE CRYSTAL? (You can also type \"BACK\" to go back.) "
+whichenemy = "Which enemy is to be targeted? (You can also type \"BACK\" to go back.) "
 while True:
-    if not player.taketurn(enemy.defense):
-        break
+    for player in players:
+        if not player.taketurn([enemy.defense for enemy in enemies]):
+            break
     if defeatstate():
         break
-    enemy.taketurn(player.defense)
+    for enemy in enemies:
+        enemy.taketurn([player.defense for player in players])
     if defeatstate():
         break
-    hpstatistics()
+    hpstatistics(enemies,players)
